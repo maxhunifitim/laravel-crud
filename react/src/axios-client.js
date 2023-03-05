3d34547957a6b49728f11dbc00ffa9ab4ddeb5b5
Fitim Maxhuni <fmaxhuni57@gmail.com>
2023-03-05 16:10:48 +0100

    Update axios-client.js

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -4,21 +4,4 @@ const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
-axiosClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        try {
-            const { response } = error;
-            if (response.status === 401) {
-                localStorage.removeItem("ACCESS_TOKEN");
-            }
-        } catch (e) {
-            console.error(e);
-        }
-        throw error;
-    }
-);
-
 export default axiosClient;
